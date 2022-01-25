3bb53f4b6b9e9e6f9225404b273ea6a1601c2166
bulluhemanth <bulluhemanth@gmail.com>
2022-01-25 23:02:17 +0530

    //code clean up

--- pwd_gen.js
+++ pwd_gen.js
@@ -36,15 +36,4 @@ function show_value(x) {
   // alert("Copied the text: " + copyText);
 }
 
-function copyToClipboard(text) {
-  var dummy = document.createElement("newpass");
-  // to avoid breaking orgain page when copying more words
-  // cant copy when adding below this code
-  // dummy.style.display = 'none'
-  document.body.appendChild(dummy);
-  //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-  dummy.value = text;
-  dummy.select();
-  document.execCommand("copy");
-  document.body.removeChild(dummy);
-}
+
